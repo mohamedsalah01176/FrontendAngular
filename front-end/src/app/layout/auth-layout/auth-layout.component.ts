742a22c80892544dc73a48d13501658cb30d3332
“M0hamedMamd0uh” <mohamedm190099@gmail.com>
2025-04-30 18:43:51 +0300

    change Images to images v3

--- front-end/src/app/layout/auth-layout/auth-layout.component.ts
+++ front-end/src/app/layout/auth-layout/auth-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthNavbarComponent } from "../../component/auth-navbar/auth-navbar.component";
+import { FooterComponent } from "../../component/footer/footer.component";
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterOutlet, AuthNavbarComponent],
+  imports: [RouterOutlet, AuthNavbarComponent, FooterComponent],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.css'
 })
