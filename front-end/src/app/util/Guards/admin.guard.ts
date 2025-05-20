35b42b4f7c19d33298a9ead06b807743bd5a7ba6
abdelrhmanabdelhaleem <abdelrhmman123@gmail.com>
2025-05-20 17:41:27 +0300

    apply guard

--- front-end/src/app/util/Guards/admin.guard.ts
+++ front-end/src/app/util/Guards/admin.guard.ts
@@ -1,10 +1,15 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
 
-export const authGuard: CanActivateFn = (route, state) => {
+interface DecodedToken {
+  role: string;
+}
+
+export const adminGuard: CanActivateFn = (route, state) => {
   const _Router = inject(Router);
   const _AuthService = inject(AuthService);
   const platformId = inject(PLATFORM_ID);
@@ -12,12 +17,17 @@ export const authGuard: CanActivateFn = (route, state) => {
   if (isPlatformBrowser(platformId)) {
     const token = _AuthService.getTokenFromCookies();
     if (token) {
-      return true;
+      const user = jwtDecode<DecodedToken>(token);
+      if (user.role === 'admin') {
+        return true;
+      } else {
+        _Router.navigate(['/home']);
+        return false;
+      }
     } else {
       _Router.navigate(['/login']);
       return false;
     }
   }
-
   return false;
 };
